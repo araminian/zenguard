7f31eef26b276d4ef8fdbadf1069efadc20b787d
araminian <rmin.aminian@gmail.com>
2022-01-19 10:55:12 +0000

    [Utils/K8s/delete] Delete ConfigMap

---
+++ zenguard/utils/k8s/delete.py
@@ -0,0 +1,15 @@
+from kubernetes import client
+from typing import Union
+
+def deleteConfigMap(name:str,namespace:str) -> Union[bool,dict]:
+    v1API = client.CoreV1Api()
+    try:
+        v1API.delete_namespaced_config_map(
+            name=name,
+            namespace=namespace
+        )
+        
+    except client.exceptions.ApiException as e:
+        return {"ErrorCode":'600',"ErrorMsg": "Can not delete configMap {0} , reason {1}".format(name,e.reason)}
+    
+    return True
